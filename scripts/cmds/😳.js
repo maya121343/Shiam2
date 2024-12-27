87ab429f770fc385fc340da9601275a739d4ea41
shiam21 <mdsohelrana23293@gmail.com>
2024-12-27 17:02:44 +0600

    Update 😳.js

--- scripts/cmds/😳.js
+++ scripts/cmds/😳.js
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "😳") {
  return message.reply({
- body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐀𝐁𝐇𝐑𝐀𝐍𝐈𝐋☆\nᑘᒪᒪᗩSᕼ ッ」",
+ body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n☆𝐬𝐡𝐢𝐚𝐦★~(◠‿◕✿)☆\nᑘᒪᒪᗩSᕼ ッ」",
  attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=17Ujd7EJ9K-ZW6-dUNi4JkR8wg-_Xja0P")
  });
  }
