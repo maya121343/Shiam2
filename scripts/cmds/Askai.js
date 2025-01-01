1ca8d66b962706d0c2210576c39ae26653481530
shiam21 <mdsohelrana23293@gmail.com>
2025-01-01 16:27:47 +0600

    Create Askai.js

---
+++ scripts/cmds/Askai.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+const Prefixes = [
+  'saima2', 
+  'ai2'
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "Shiam",
+    longDescription: "AI", 
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+      
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply(" assalamualaikum im saima,,, chatgpt version 2 how can i help you? ");
+        return;
+      }
+
+
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+ 
+    await message.reply({ body: 'thinking..'
+━━━━━━━━━━━━━        
+${answer}
+━━━━━━━━━━━━━`,
+});
+
+   } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
